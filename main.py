45d52ece15782355018b7502d6341e11e3b61aea
Madi Muratuly <Madi.Muratuly@aaengineering.kz>
2019-11-26 11:53:25 +0600

    2 commit

--- main.py
+++ main.py
@@ -72,11 +72,31 @@ def longestPalindromeSubseq(s):
         return d[s]
     return f(s)
 
+
+def leetcode(s):
+    def bt(start, current):
+        if len(current) == 4:
+            if start == len(s):
+                result.append('.'.join(current))
+            return
+        for i in range(start + 1, min(start + 4, len(s) + 1)):
+            if i - 1 > start and s[start] == '0':
+                continue
+            a = s[start:i]
+            if 0 <= int(a) <= 255:
+                current.append(a)
+                bt(i, current)
+                current.pop()
+    result = []
+    bt(0, [])
+    return result
+
 if __name__ == '__main__':
     #leetcode3()
     #print(leetcode5("babad"))
     #print(leetcode516("asdasdasdaaaaasdd"))
-    print(longestPalindromeSubseq("asdasdasdaaaaasdd"))
+    #print(longestPalindromeSubseq("asdasdasdaaaaasdd"))
+    print(leetcode("25525511133"))
 
 
     #print('asdasd')
